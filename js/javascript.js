83f853bdf5f39276c935ce5ba6b930302f039c0c
40457712 <40457712@live.napier.ac.uk>
2022-03-12 17:04:43 +0000

    Changes to the alert text

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,3 @@
 function show() {
-    alert("Auto Play of the sound is turned off by default!\n Turn on the sound by clicking on the play button\n to get the full effect");
+    alert("Auto Play of the sound is turned off by default!\n To turn on the sound\n Click play on the white control\n TOP LEFT of the screen\n to get the full effect Star Wars effect.....\n May the force be with you!");
  }
