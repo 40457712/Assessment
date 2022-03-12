d15f42957bceded118127a040da256f65eb57782
40457712 <40457712@live.napier.ac.uk>
2022-03-12 16:34:02 +0000

    Change text in alert

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,3 @@
 function show() {
-    alert("Remember to turn on the sound by clicking on the play button");
+    alert("Auto Play of the sound is turned off by default!\n Turn on the sound by clicking on the play button\n to get the full effect");
  }
