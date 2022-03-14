d3ba4026ffc9c7272365707a717174309e9ed175
40457712 <40457712@live.napier.ac.uk>
2022-03-14 23:41:04 +0000

    Add two functions

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,10 @@ function show() {
     alert("Auto Play of the sound is turned off by default!\n To turn on the sound\n Click play on the white control\n TOP LEFT of the screen\n to get the full effect Star Wars effect.....\n May the force be with you!");
  }
 
- function submit() {
+ function rebel() {
      alert("You have choosen Rebel Alliance")
  }
+
+ function imperial() {
+    alert("You have choosen Imperial Darkside")
+}
