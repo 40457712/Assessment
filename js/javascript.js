37b7f718156f303b889ad3251f0e3288d9f2ec03
40457712 <40457712@live.napier.ac.uk>
2022-03-15 11:49:44 +0000

    declare varible swcharacter

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+const swcharacter = ""
+
 function show() {
     alert("Auto Play of the sound is turned off by default!\n To turn on the sound\n Click play on the white control\n TOP LEFT of the screen\n to get the full effect Star Wars effect.....\n May the force be with you!");
  }
