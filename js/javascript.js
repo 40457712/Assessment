056e32e5b257d1e4a3f7a7d5cd45118da2b20003
40457712 <40457712@live.napier.ac.uk>
2022-03-28 17:14:53 +0100

    Add semicolon

--- js/javascript.js
+++ js/javascript.js
@@ -8,12 +8,12 @@ function show() {
  }
 
  function rebel() {
-    document.getElementById('characterI').innerHTML = ""
+    document.getElementById('characterI').innerHTML = "";
     document.getElementById('characterR').innerHTML = '<img src="https://40457712.github.io/assessment/images/c3po290X590.jpg" onclick="change(&#34;C3PO&#34;)" alt="c3po" width="100" height="100"><img src="https://40457712.github.io/assessment/images/chewbacca290X290.png" onclick="change(&#34;chewbacca&#34;)" alt="chewbacca" width="100" height="100">';
 }
 
  function imperial() {
-    document.getElementById('characterR').innerHTML = ""
+    document.getElementById('characterR').innerHTML = "";
     document.getElementById('characterI').innerHTML = '<img src="https://40457712.github.io/assessment/images/bobafett290X290.png" onclick="change(&#34;bobafett&#34;)" alt="bobafett" width="100" height="100"><img src="https://40457712.github.io/assessment/images/darthmaul290X290.jpg" onclick="change(&#34;darthmarl&#34;)" alt="darthmarl" width="100" height="100">';
 }
 
