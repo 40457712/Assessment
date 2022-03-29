6b751805b56bf29594e86d6ed94fca9c91ee72fa
40457712 <40457712@live.napier.ac.uk>
2022-03-29 09:32:53 +0100

    Add qcount to next function

--- js/javascript.js
+++ js/javascript.js
@@ -60,7 +60,7 @@ function next() {
       var ansr = document.getElementsByName('answer');   
             for(i = 0; i < ansr.length; i++) {
                 if(ansr[i].checked)
-                  alert("You Answered: " + ansr[i].value + " - Correct Answer: " + quiz[Qcount][3]);
+                  alert("You Answered: " + ansr[i].value + " - Correct Answer: " + quiz[Qcount][3] + "Q" + Qcount);
                   // if(ansr[i].value == quiz[Qcount][3])
                   // alert("You got the correct answer!");
       }
