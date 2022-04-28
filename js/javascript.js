4792cdeee57391197aeeb343880edb7f18f922fa
40457712 <40457712@live.napier.ac.uk>
2022-04-28 23:48:50 +0100

    Upload change to name textbox

--- js/javascript.js
+++ js/javascript.js
@@ -72,6 +72,8 @@ function show() {
 // Start function to make main screen visible
 
 function start() {
+   var x = document.getElementById("inputname").value;
+   alert("Your name is " + x);
    var setup1 = document.getElementById("settings");
    // alert("Visiblity of settings is " + setup1.style.visibility);
    setup1.style.visibility = "hidden";
